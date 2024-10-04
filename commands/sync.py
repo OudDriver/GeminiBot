076148417c6f0bf11fffdfb918f61c1efd34a9fe
OudDriver <d.japaryo@gmail.com>
2024-10-04 23:19:37 +0700

    I thought the sync command worked all this time.

--- commands/sync.py
+++ commands/sync.py
@@ -7,7 +7,7 @@ with open('config.json') as f:
 @commands.hybrid_command()
 async def sync(ctx: commands.Context):
     """Syncs slash commands."""
-    if ctx.author.id == int(config.get('owner_id', 0)):
+    if str(ctx.author.id) == str(config['OwnerID']):
         await ctx.send(f'Syncing...')
         synced = await ctx.bot.tree.sync()
         await ctx.send(f'Synced {len(synced)} Command(s): {synced}')

c1676134ea7d4f825400084dfa073d5eaebc10f6
OudDriver <d.japaryo@gmail.com>
2025-04-12 22:33:04 +0700

    Added support for Gemini 2.0 Flash Image Generation May need to modify next time as Gemini may support tools and system instructions for its image generation model. Also, I realized that contains_code_exec is absolutely useless.

--- commands/prompt.py
+++ commands/prompt.py
@@ -13,6 +13,7 @@ from google.genai.types import GenerateContentConfig, SafetySetting, Part,\
     Tool
 from google.genai import Client
 
+from bot.setup import load_config
 from packages.utils import clean_text, create_grounding_markdown, send_long_messages, \
     send_long_message, send_image, generate_unique_file_name, repair_links, save_temp_config
 from packages.tex import render_latex, split_tex, check_tex
@@ -21,11 +22,10 @@ from packages.file_utils import handle_attachment, wait_for_file_active
 from packages.memory import load_memory
 from packages.maps import BLOCKED_CATEGORY, HARM_PRETTY_NAME
 
-CONFIG = json.load(open("config.json"))
-
 YOUTUBE_PATTERN = regex.compile(r'(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)('
                                 r'?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)')
 MAX_MESSAGE_LENGTH = 2000
+CONFIG = load_config()
 SAFETY_SETTING = CONFIG["HarmBlockThreshold"]
 
 latest_token_count = 0
@@ -66,26 +66,55 @@ def prompt(tools: list[Tool], genai_client: Client):
             now = datetime.now(timezone.utc)
             formatted_time = now.strftime("%A, %B %d, %Y %H:%M:%S UTC")
             
-            with open("temp/temp_config.json", "r") as TEMP_CONFIG:
+            with open("temp/temp_config.json") as TEMP_CONFIG:
                 configs = json.load(TEMP_CONFIG)
             
             model = configs['model']
             safety_settings = [
-                SafetySetting(category=HarmCategory.HARM_CATEGORY_CIVIC_INTEGRITY, threshold=SAFETY_SETTING),
-                SafetySetting(category=HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold=SAFETY_SETTING),
-                SafetySetting(category=HarmCategory.HARM_CATEGORY_HARASSMENT, threshold=SAFETY_SETTING),
-                SafetySetting(category=HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold=SAFETY_SETTING),
-                SafetySetting(category=HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold=SAFETY_SETTING)
+                SafetySetting(
+                    category=HarmCategory.HARM_CATEGORY_CIVIC_INTEGRITY,
+                    threshold=SAFETY_SETTING,
+                ),
+                SafetySetting(
+                    category=HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+                    threshold=SAFETY_SETTING,
+                ),
+                SafetySetting(
+                    category=HarmCategory.HARM_CATEGORY_HARASSMENT,
+                    threshold=SAFETY_SETTING,
+                ),
+                SafetySetting(
+                    category=HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+                    threshold=SAFETY_SETTING,
+                ),
+                SafetySetting(
+                    category=HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+                    threshold=SAFETY_SETTING,
+                ),
             ]
 
+            if model == "gemini-2.0-flash-exp-image-generation":
+                response_modalities = ["Text", "Image"]
+                tool_func_call = None
+                auto_func_call = None
+                system_instructions = None
+
+            else:
+                response_modalities = None
+                tool_func_call = tools
+                auto_func_call = AutomaticFunctionCallingConfig(maximum_remote_calls=5)
+                system_instructions = configs['system_prompt']
+
             config = GenerateContentConfig(
-                system_instruction=configs['system_prompt'],
-                tools=tools,
+                system_instruction=system_instructions,
+                tools=tool_func_call,
                 safety_settings=safety_settings,
-                automatic_function_calling=AutomaticFunctionCallingConfig(maximum_remote_calls=5)
+                automatic_function_calling=auto_func_call,
+                response_modalities=response_modalities,
+                temperature=0.3
             )
 
-            async with ((ctx.typing())):
+            async with ctx.typing():
                 if message.lower() == "{clear}":
                     if ctx.author.guild_permissions.administrator:
                         memory.clear()
@@ -116,7 +145,7 @@ def prompt(tools: list[Tool], genai_client: Client):
                 if ctx.message.attachments and not hasattr(results[0], "__getitem__"):
                     await send_long_message(
                         ctx,
-                        f"Error: Failed to upload attachment(s). Continuing as if the files are not uploaded. `{results[0]}`",
+                        f"Error: Failed to upload attachment(s). Continuing as if the files are not uploaded. Error: `{results[0]}`.",
                         MAX_MESSAGE_LENGTH
                     )
                 else:
@@ -157,14 +186,15 @@ def prompt(tools: list[Tool], genai_client: Client):
                         logging.info(f"Found and Processed Link {url}")
                         final_prompt.append(Part(file_data=file))
 
+                if model == "gemini-2.0-flash-exp-image-generation":
+                    final_prompt = final_prompt[-1]
+
                 logging.info(f"Got Final Prompt {final_prompt}")
 
                 response = await chat.send_message(final_prompt)
 
-                logging.info(f"Received Initial Message {response.text}")
-
                 if response.candidates[0].finish_reason == FinishReason.MALFORMED_FUNCTION_CALL:
-                    logging.error(f"Function call is malformed!")
+                    logging.error("Function call is malformed!")
                     await ctx.reply("Seems like my function calling tool is malformed. Try again!")
                     return
 
@@ -181,16 +211,15 @@ def prompt(tools: list[Tool], genai_client: Client):
 
                 latest_token_count = response.usage_metadata.total_token_count
 
-                contains_code_exec = False
-                for part in response.candidates[0].content.parts:
-                    if part.executable_code is not None or part.code_execution_result is not None or part.inline_data is not None:
-                        contains_code_exec = True
-                        break
-
                 memory = chat._curated_history
 
-                async def handle_text_only_messages():
-                    text, thought_matches, secret_matches = clean_text(response.text)
+                async def handle_text_only_messages(text):
+                    text, thought_matches, secret_matches = clean_text(text)
+
+                    if configs['uwu']:
+                        uwu = Uwuifier()
+                        text = uwu.uwuify_sentence(text)
+
                     if thought_matches:
                         save_temp_config(thought=thought_matches)
                         text += "\n(This reply have a thought)"
@@ -198,16 +227,8 @@ def prompt(tools: list[Tool], genai_client: Client):
                     if secret_matches:
                         save_temp_config(secret=secret_matches)
 
-                    google_search = any(
-                        not callable(tool) and tool.google_search for tool in tools
-                    )
-
-                    if google_search:
+                    if any(not callable(tool) and tool.google_search for tool in tools):
                         text = text + f"\n{create_grounding_markdown(response.candidates)}"
-
-                    if configs['uwu']:
-                        uwu = Uwuifier()
-                        text = uwu.uwuify_sentence(text)
 
                     if response.candidates[0].finish_reason == FinishReason.MAX_TOKENS:
                         text = text + "\n(Response May Be Cut Off)"
@@ -225,41 +246,41 @@ def prompt(tools: list[Tool], genai_client: Client):
                                 file_names.append(file_tex)
                                 response_if_tex[j] = discord.File(file_tex)
 
-                        await send_long_messages(ctx, response_if_tex, MAX_MESSAGE_LENGTH)
+                        await send_long_messages(
+                            ctx, response_if_tex, MAX_MESSAGE_LENGTH
+                        )
                     else:
                         await send_long_message(ctx, text, MAX_MESSAGE_LENGTH)
 
                     logging.info(f"Sent\nText:\n{text}\nThought:\n{thought_matches}\nSecrets:\n{secret_matches}")
 
-                if contains_code_exec:
-                    for part in response.candidates[0].content.parts:
-                        logging.info(f"Got Code Execution Response.")
-                        if part.text is not None:
-                            await handle_text_only_messages()
-                        if part.executable_code is not None:
-                            logging.info("Ran Code Execution:\n" + part.executable_code.code)
-                            await send_long_message(ctx,
-                                                    f"Code:\n```{part.executable_code.language.name.lower()}\n{part.executable_code.code}\n```",
-                                                    MAX_MESSAGE_LENGTH)
-                        if part.code_execution_result is not None:
-                            logging.info(f"Code Execution Output:\n{part.code_execution_result.output}")
-                            await send_long_message(ctx,
-                                                    f"Output:\n```\n{part.code_execution_result.output}\n```",
-                                                    MAX_MESSAGE_LENGTH)
-                        if part.inline_data is not None: 
-                            logging.info("Got Code Execution Image")
-                            file_name = './temp/' + generate_unique_file_name("png")
-                            try:
-                                with open(file_name, "wb") as f:
-                                    f.write(part.inline_data.data)
-                            except Exception as e:
-                                logging.error(e)
+                logging.info("Got Response.")
+                for part in response.candidates[0].content.parts:
+                    if part.executable_code is not None:
+                        logging.info("Ran Code Execution:\n" + part.executable_code.code)
+                        await send_long_message(ctx,
+                                                f"Code:\n```{part.executable_code.language.name.lower()}\n{part.executable_code.code}\n```",
+                                                MAX_MESSAGE_LENGTH)
 
-                            file_names.append(file_name)
+                    if part.code_execution_result is not None:
+                        logging.info(f"Code Execution Output:\n{part.code_execution_result.output}")
+                        await send_long_message(ctx,
+                                                f"Output:\n```\n{part.code_execution_result.output}\n```",
+                                                MAX_MESSAGE_LENGTH)
 
-                            await send_image(ctx, file_name)
-                else:
-                    await handle_text_only_messages()
+                    if part.inline_data is not None:
+                        logging.info("Got Image")
+                        file_name = './temp/' + generate_unique_file_name("png")
+                        try:
+                            with open(file_name, "wb") as f:
+                                f.write(part.inline_data.data)
+                        except Exception as e:
+                            logging.error(e)
+                        file_names.append(file_name)
+                        await send_image(ctx, file_name)
+
+                    if part.text is not None:
+                        await handle_text_only_messages(part.text)
 
         except ssl.SSLEOFError as e:
             error_message = (f"A secure connection error occurred (SSL connection unexpectedly closed)."

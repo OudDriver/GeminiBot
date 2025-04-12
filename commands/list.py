ad045bbc066a88bc086093ef813ff23279261479
OudDriver <d.japaryo@gmail.com>
2025-04-12 22:27:57 +0700

    Added more meaningful messages for list command

--- commands/list.py
+++ commands/list.py
@@ -6,14 +6,25 @@ def setup_list_command(client, initial_state):
     async def list_sys(ctx: commands.Context):
         """Lists all system prompts"""
         all_sys_dict = initial_state['system_prompts']
+        all_tools_dict = initial_state['tools']
+
         all_sys = []
+        all_models = list(initial_state["model_clean_names"].values()) # It's an array!
+        all_tools = list(all_tools_dict)
+
         for i in all_sys_dict:
             all_sys.append(i['Name'])
 
-        formatted_list = ""
-        if len(all_sys) > 1:
-            formatted_list = ', '.join(all_sys[:-1]) + ', and ' + all_sys[-1]
-        else:
-            formatted_list = all_sys[0] if all_sys else "none"
+        formatted_string_sys = ''
+        for i, v in enumerate(all_sys):
+            formatted_string_sys += f"\n{i + 1}: {v}"
 
-        await ctx.reply(f"The system prompts include: {formatted_list}.", ephemeral=True)
+        formatted_string_models = ""
+        for i, v in enumerate(all_models):
+            formatted_string_models += f"\n{i + 1}: {v}"
+
+        formatted_string_tools = ""
+        for i, v in enumerate(all_tools):
+            formatted_string_tools += f"\n{i + 1}: {v}"
+
+        await ctx.reply(f"The system prompts include: {formatted_string_sys}\n\nThe models include: {formatted_string_models}\n\nThe tools include: {formatted_string_tools}", ephemeral=True)
